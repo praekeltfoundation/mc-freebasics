265da94fe32cb16b199c86c63f9234e25262ee6d
Milton Madanda <madandat@gmail.com>
2016-03-10 22:14:05 +0200

    rename migrations

--- freebasics/migrations/0005_remove_selected_template_field.py
+++ freebasics/migrations/0005_remove_selected_template_field.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('freebasics', '0002_freebasicstemplatedata'),
+        ('freebasics', '0004_remove_freebasicstemplatedata_color'),
     ]
 
     operations = [
--- freebasics/migrations/0006_change_site_url_field_type.py
+++ freebasics/migrations/0006_change_site_url_field_type.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('freebasics', '0003_remove_selected_template_field'),
+        ('freebasics', '0005_remove_selected_template_field'),
     ]
 
     operations = [
